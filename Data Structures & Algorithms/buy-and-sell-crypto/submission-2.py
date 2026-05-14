9a7c2b2651536c68849e252c67ca77dac704723e
Aref Turkieh <turkieharef@gmail.com>
2026-05-13 21:34:13 -0400

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        minimum = prices[0]
+        best_profit = 0
+
+        for price in prices:
+            if price <= minimum:
+                minimum = price
+            else:
+                profit = price - minimum
+                if profit > best_profit:
+                    best_profit = profit
+        return best_profit
+
+        
+
+
+        
